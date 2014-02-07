cbcb82cc0ec3c6fa3418323bcaa71184227fab5a
Matheus <oliveira.matheusde@gmail.com>
2014-02-07 15:38:35 -0200

    update wsgi config

--- eventex/wsgi.py
+++ eventex/wsgi.py
@@ -11,7 +11,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventex.settings")
 
 # Staticfiles
+from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-from django.core.wsgi import get_wsgi_application
 application = Cling(get_wsgi_application())
