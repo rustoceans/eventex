4a48be77eabb5cbfc8caf21f25f65fd5cc8da109
matheus <oliveira.matheusde@gmail.com>
2014-04-08 18:16:33 -0300

    Deploy

--- eventex/settings.py
+++ eventex/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import sys
 import unipath
 from decouple import config
 from dj_database_url import parse as db_url
